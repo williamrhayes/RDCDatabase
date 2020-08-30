41261653326f60f888c9896c53cdb9231cd0a461
Billy Hayes <67700421+williamrhayes@users.noreply.github.com>
2020-08-29 20:39:47 -0500

    Add files via upload
    Finished building the first version of the automated Excel to SQL database python program! This does not automate everything, there is still data that we will insert manually into our tables. These tables can be added to manually because they are not updated as frequently as orders are. This program DOES take the grunt work out of adding hundreds of rows of frequently updating data!

--- py_2_SQL.py
+++ py_2_SQL.py
@@ -1,6 +1,5 @@
 import mysql.connector
 import pandas as pd
-import re
 
 
 def main():
@@ -13,7 +12,7 @@ def main():
     mydb = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='ratehufapacc',
+        password='password',
         database='rdc'
     )
 
@@ -30,6 +29,7 @@ def main():
 
     update_customers(mydb, c, df_dict)
     update_business_expenses(mydb, c, df_dict)
+    update_orders(mydb, c, df_dict)
 
 
 # This function returns a dictionary of pandas dataframes based
@@ -59,7 +59,8 @@ def update_customers(mydb, c, df_dict):
         customer_name_list = list(filter(None, row['Unnamed: 1'].split(' ')))
         name_string = (customer_name_list[0] + customer_name_list[-1]).lower()
         if name_string not in current_customers:
-            customer_id = 'CU' + f'{(len(current_customers)+1):04d}'
+            next_highest_key = int(present_data[-1][0][2:]) + 1
+            customer_id = 'CU' + f'{next_highest_key:04d}'
             current_customers.append(name_string)
 
             first_name = customer_name_list[0]
@@ -90,8 +91,10 @@ def update_business_expenses(mydb, c, df_dict):
     # see if entries are already there
     c.execute('SELECT * FROM business_expenses')
     present_data = [row for row in c.fetchall()]
+
     current_expenses = [(str(row[4]) + row[1] + row[2]).lower()
                         for row in present_data]
+
     # Gather and clean data from our Excel Spreadsheet
     df_expenses = df_dict['Costs']
     df_expenses = (df_expenses.drop([0,1]))
@@ -106,7 +109,8 @@ def update_business_expenses(mydb, c, df_dict):
                        ).lower()
 
         if expense_tag not in current_expenses:
-            expense_id = 'EXP' + f'{(len(current_expenses)+1):04d}'
+            next_highest_key = int(present_data[-1][0][3:]) + 1
+            expense_id = 'EXP' + f'{next_highest_key:04d}'
             current_expenses.append(expense_tag)
             expense = [expense_id, row['Supplier'], row['ItemName'],
                        row['TotalCost'], str(row['DatePurchased'].date()), None]
@@ -115,9 +119,73 @@ def update_business_expenses(mydb, c, df_dict):
 
     insert_to_SQL(mydb, c, 'business_expenses', new_expenses)
 
-
+# Scans the Excel spreadsheet and looks for new
+# Orders to add to our SQL database
 def update_orders(mydb, c, df_dict):
-    pass
+    c.execute('SELECT * FROM orders')
+    present_data = [row for row in c.fetchall()]
+    #               customer_id + product_id + date
+    current_orders = [(row[1] + row[2] + str(row[3])).lower()
+                      for row in present_data]
+
+    # Our orders table will be more complicated. The Orders table will require
+    # information from other SQL tables and multiple Excel sheets.
+
+    df_transactions = df_dict['Transactions']
+    df_transactions = df_transactions.drop([0, 1])
+
+
+
+    new_orders = []
+    for index, row in df_transactions.iterrows():
+        # Retrieve Customer ID
+        customer_names = list(filter(None, row['Unnamed: 1'].split(' ')))
+        cmnd = ('SELECT * FROM customers ' \
+                  "WHERE customers.first_name=%s " \
+                  "AND customers.last_name=%s")
+        c.execute(cmnd, (customer_names[0], customer_names[-1]))
+        customer_id = c.fetchone()[0]
+
+        #Retrieve Product ID
+        product_name = row['Unnamed: 7'].replace('Coffee', '')
+        cmnd = ('SELECT * FROM products ' \
+                "WHERE products.product_name='" + product_name + "'")
+        c.execute(cmnd, product_name)
+        product_info = c.fetchone()
+
+        if product_info is None:
+            print("\nOrder for customer ",customer_id, "on",
+                  row['Transactions'].date(),"needs review,")
+            print('Product: "' + product_name + '" not found.\n')
+        else:
+            product_id = product_info[0]
+
+            #Retrieve Order Date
+            order_date = row['Transactions'].date()
+
+            # Find the key for this order
+            if len(present_data) == 0:
+                next_highest_key = 1
+            else:
+                next_highest_key = int(present_data[-1][0][3:]) + 1
+
+            order_tag = (customer_id + product_id + str(order_date)).lower()
+            if order_tag not in current_orders:
+
+                if len(present_data) == 0:
+                    order_id = 'ORD' + f'{(len(current_orders)+1):04d}'
+                else:
+                    order_id = 'ORD' + f'{next_highest_key:04d}'
+                current_orders.append(order_tag)
+                order = (order_id, customer_id, product_id,
+                        order_date, product_name, row['Unnamed: 6'],
+                        row['Unnamed: 13'], row['Unnamed: 8'],
+                        row['Unnamed: 8'] * 0.065, # Calculate sales tax
+                        0.00, None, None)
+
+                new_orders.append(tuple(order))
+
+    insert_to_SQL(mydb, c, 'orders', new_orders)
 
 
 # This function takes a list of tuples and inserts them
@@ -129,9 +197,10 @@ def insert_to_SQL(mydb, cursor, table_name, values):
         cmnd = "INSERT INTO " + table_name + " " \
                 "VALUES (" + "%s,"*(len(values[0])-1) + "%s);"
         cursor.executemany(cmnd, values)
-        mydb.commit()
-        print(cursor.rowcount, "new rows added to " + table_name + ".")
 
+        #mydb.commit()
+
+        print(cursor.rowcount, "new rows added to " + table_name + ".")
 
 main()
 
